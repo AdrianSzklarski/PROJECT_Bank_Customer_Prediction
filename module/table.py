1792df3433d7c869d10be8e6d529542185ce72a5
AdrianSzklarski <adrian_it@o2.pl>
2023-06-08 10:48:23 +0200

    Cleared of CSV

--- module/table.py
+++ module/table.py
@@ -33,9 +33,9 @@ class DataCSV:
 
         for i in range(1, 101):
             '''Create data to csv file'''
-            customer_number = np.random.randint(100000000, 999999999, 1)
+            customer_number = int(np.random.randint(100000000, 999999999, 1))
             customer_age = np.random.randint(18, 64)
-            number_of_counts = np.random.randint(1, 6, 1)
+            number_of_counts = int(np.random.randint(1, 6, 1))
             earnings = np.random.randint(2500, 32000)
 
             WORDS = ('male', 'female')
@@ -50,19 +50,19 @@ class DataCSV:
 
             if 2500 <= earnings < 5000:
                 card_category = 'White'
-                credit_limit = 2500 * 1.5
+                credit_limit = round(2500 * 1.5, 2)
             elif 5000 <= earnings < 7500:
                 card_category = 'Black'
-                credit_limit = 5000 * 1.58
+                credit_limit = round(5000 * 1.58, 2)
             elif 7500 <= earnings < 12000:
                 card_category = 'Silver'
-                credit_limit = 7500 * 1.82
+                credit_limit = round(7500 * 1.82, 2)
             elif 12000 <= earnings <= 18000:
                 card_category = 'Gold'
-                credit_limit = 12000 * 2.05
+                credit_limit = round(12000 * 2.05, 2)
             else:
                 card_category = 'Platinum'
-                credit_limit = 18000 * 2.47
+                credit_limit = round(18000 * 2.47, 2)
 
             self.df = pd.DataFrame(data=[[customer_number, customer_age, gender, number_of_counts,
                                           education_level, marital_status, f'{earnings} zł brutto',
@@ -79,15 +79,31 @@ class DataCSV:
 
         link_csv = '/home/adrian/Pulpit/GitHub_Public/Bank_Customers_Prediction/csv/filename.csv'
         link_csv_new = '/home/adrian/Pulpit/GitHub_Public/Bank_Customers_Prediction/csv/filename_new.csv'
+
         with open(link_csv, 'r') as f:
+
+            # delete first column
+            df = pd.read_csv(link_csv)
+            first_column = df.columns[0]
+            df = df.drop([first_column], axis=1)
+            df.to_csv(link_csv, index=False)
+
             line_count = 0
             for i in f:
-                if line_count == 0:
+
+                if line_count == 0:   # Head
                     head = f'{"".join(i)}'
+                    comma = head.translate({ord(','): None}).replace('Customer number', 'Customer number, ') \
+                        .replace('Customer age', 'Customer age, ').replace('Gender', 'Gender, ') \
+                        .replace('Number of counts', 'Number of counts, ') \
+                        .replace('Education level', 'Education level, ') \
+                        .replace('Marital status', 'Marital status, ') \
+                        .replace('Earnings', 'Earnings, ').replace('Card Category', 'Card Category, ')
                     with open(link_csv_new, 'a', newline="", encoding='UTF-8') as csv_file_new:
-                        csv_file_new.write(head)
+                        csv_file_new.write(comma)
                     line_count += 1
-                elif line_count % 2 == 0:
+
+                elif line_count % 2 == 0:  # Lines
                     with open(link_csv_new, 'a', newline="", encoding='UTF-8') as csv_file_new:
                         csv_file_new.write(i)
                 line_count += 1
