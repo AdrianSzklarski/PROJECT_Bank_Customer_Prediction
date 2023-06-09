48cde6c8487ece87ec60b946f073f3422c0db762
AdrianSzklarski <adrian_it@o2.pl>
2023-06-09 10:31:39 +0200

    Added main file of program 'Bank' & bilded Class & cheak CSV data

---
+++ module/bank.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+linkCSV = pd.read_csv('/home/adrian/Pulpit/GitHub_Public/Bank_Customers_Prediction/csv/filename_new.csv')
+
+
+class Bank:
+    def __init__(self, linkCSV):
+        self.linkCSV = linkCSV
+
+    def get_test(self):
+        '''Code testing module'''
+        return self.linkCSV.head(50)
+
+    def __str__(self):
+        return str(self.get_test())
+
+
+if __name__ == '__main__':
+    result = Bank(linkCSV)
+    print(result)
